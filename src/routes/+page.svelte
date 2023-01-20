c9d8cef87b8f7be3c7747cf555265a2a19f02b62
w3cdp6084-dev <yusukew3@gmail.com>
2023-01-20 19:57:45 +0900

    top page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { PageData } from "./$types";
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>ブログ</h1>
+  <ul>
+    {#each data.contents as content}
+      <li><a href="{content.id}">{content.title}</a></li>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
