88197e807b05993c65c05faa72dfaeb791845fd1
w3cdp6084-dev <yusukew3@gmail.com>
2023-01-23 12:36:44 +0900

    Headerの中にMenu add

--- src/components/layouts/Header.svelte
+++ src/components/layouts/Header.svelte
@@ -1,13 +1,12 @@
+<script>
+    import { Hamburger } from 'svelte-hamburgers';
+    import Menu from './Menu.svelte';
+    let open;
+</script>
+
 <header class="l-header">
-	<p class="l-header__logo"><a href="/" class="l-header__logo-link">ロゴ</a></p>
-	<nav class="l-nav">
-		<ul class="l-nav-list">
-			<li class="l-nav-list__item">
-				<a href="/" class="l-nav-list__item-link">TOP</a>
-			</li>
-			<li class="l-nav-list__item">
-				<a href="/blog/" class="l-nav-list__item-link">ブログ</a>
-			</li>
-		</ul>
-	</nav>
+	<Hamburger
+    	bind:open
+    	--color="black" />
+	<Menu bind:open />
 </header>
