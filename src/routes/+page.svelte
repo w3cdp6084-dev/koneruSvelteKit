c54f9fdfecaa483f44e66a82de35dd8b3f24f1a7
w3cdp6084-dev <yusukew3@gmail.com>
2023-01-25 12:43:12 +0900

    emoji

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
   import type { PageData } from "./$types";
   import ThemeSwitch from '$lib/ThemeSwitch/ThemeSwitch.svelte';
   import { Card} from "flowbite-svelte";
-  let vCard = false;
   export let data: PageData;
   
 </script>
@@ -21,6 +20,7 @@
         <Card img={content.eyecatch?.url} class="item max-w-lg">
           <a href="{content.id}" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{content.title}</a><br>
           <a href="{content.id}" class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{content.category.name}</a>
+          <p class="mb-2 text-gray-700 dark:text-gray-400 leading-tight">{content.createdAt}</p>
         </Card>
       </li>
     {/each}
@@ -29,7 +29,5 @@
 
 
 <style lang="postcss">
-/* .list > .item{
-  border-radius: 22px;
-} */
+
 </style>
