c2f371f0a7ba635c27525eabdcd9f5e653175762
w3cdp6084-dev <yusukew3@gmail.com>
2023-02-04 06:20:48 +0900

    dayjs delete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
         <Card img={content.eyecatch?.url} class="item max-w-lg">
           <a href="{content.id}" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{content.title}</a><br>
           <a href="{content.id}" class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{content.category.name}</a>
-          <p class="mb-2 text-gray-700 dark:text-gray-400 leading-tight">{content.createdAt}</p>
+          <p class="mb-2 text-gray-700 dark:text-gray-400 leading-tight">{content.publishedAt}</p>
         </Card>
       </li>
     {/each}
