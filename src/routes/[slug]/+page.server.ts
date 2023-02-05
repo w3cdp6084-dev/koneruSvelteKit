b79decb2396c36ac6a6c00af5eb304002652d959
w3cdp6084-dev <yusukew3@gmail.com>
2023-02-05 16:26:37 +0900

    return

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,4 +1,4 @@
-import { getDetail } from "../../../lib/microcms";
+import { getDetail } from "../../lib/microcms";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
